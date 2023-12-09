26e62fd878432d05225c96d544541634e81441f8
kimlyy <eangkimly2020@gmail.com>
2023-12-09 16:06:54 +0700

    Dashboard

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a class="btn btn-primary" href="https://github.com/twbs/bootstrap/blob/main/.github/CONTRIBUTING.md#bug-reports" rel="noopener noreferrer" target="_blank">Read bug report guidelines</a>
+    </div>
+  );
+}
+
+export default App;
