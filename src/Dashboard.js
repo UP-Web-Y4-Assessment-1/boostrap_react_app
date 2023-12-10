a5962119284e071031e6b7aecab809543eef765d
Sengseyha HOEUNG <hoeungsengseyha168@gmail.com>
2023-12-10 17:59:28 +0700

    NavPageCollapseMenu

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,6 +4,7 @@ import LineChat from './components/LineChat';
 import PieChart from './components/PieChart';
 import NavAlerts from './components/NavAlerts';
 import NavMessages from './components/NavMessages';
+import NavPagesCollapseMenu from './components/NavPagesCollapseMenu';
 
 function Dashboard() {
     const [style, setStyle] = useState("navbar-nav bg-gradient-primary sidebar sidebar-dark accordion");
@@ -95,28 +96,8 @@ function Dashboard() {
                         <div className="sidebar-heading">
                             Addons
                         </div>
-
                         {/* <!-- Nav Item - Pages Collapse Menu --> */}
-                        <li className="nav-item">
-                            <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-                                aria-expanded="true" aria-controls="collapsePages">
-                                <i className="fas fa-fw fa-folder"></i>
-                                <span>Pages</span>
-                            </a>
-                            <div id="collapsePages" className="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                                <div className="bg-white py-2 collapse-inner rounded">
-                                    <h6 className="collapse-header">Login Screens:</h6>
-                                    <a className="collapse-item" href="login.html">Login</a>
-                                    <a className="collapse-item" href="register.html">Register</a>
-                                    <a className="collapse-item" href="forgot-password.html">Forgot Password</a>
-                                    <div className="collapse-divider"></div>
-                                    <h6 className="collapse-header">Other Pages:</h6>
-                                    <a className="collapse-item" href="404.html">404 Page</a>
-                                    <a className="collapse-item" href="blank.html">Blank Page</a>
-                                </div>
-                            </div>
-                        </li>
-
+                        <NavPagesCollapseMenu/>
                         {/* <!-- Nav Item - Charts --> */}
                         <li className="nav-item">
                             <a className="nav-link" href="charts.html">
