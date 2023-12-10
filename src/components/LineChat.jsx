e7fafd991dd7335b461435ce78c2f269cfa084ad
Sengseyha HOEUNG <hoeungsengseyha168@gmail.com>
2023-12-10 16:42:07 +0700

    LineChat

--- src/components/LineChat.jsx
+++ src/components/LineChat.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Line } from 'react-chartjs-2';
 import Chart from 'chart.js/auto';
+import { Line } from 'react-chartjs-2';
+
 
 const labels = ['January', 'February', 'March', 'April', 'May', 'June'];
 
