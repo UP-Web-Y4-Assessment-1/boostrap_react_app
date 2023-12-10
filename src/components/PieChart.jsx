0cdb73be197797170d137a29725c3ea82b414c53
Sengseyha HOEUNG <hoeungsengseyha168@gmail.com>
2023-12-10 16:43:09 +0700

    PieChart

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Pie } from 'react-chartjs-2';
 import Chart from 'chart.js/auto';
+import { Pie } from 'react-chartjs-2';
+
 
 const labels = ['January', 'February', 'March', 'April', 'May', 'June'];
 
