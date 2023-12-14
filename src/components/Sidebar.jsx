de9ad36253da1acdec8cede0fd8de05b5ffa8e21
Sengseyha HOEUNG <hoeungsengseyha168@gmail.com>
2023-12-14 23:30:54 +0700

    updateicon

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,51 +1,89 @@
-import React from 'react'
+import React from "react";
 
-function Sidebar({style , changeStyle}) {
-    return (
-        <ul className={style} id="accordionSidebar">
-                        {/* <!-- Sidebar - Brand --> */}
-                        <a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
-                            <div className="sidebar-brand-icon rotate-n-15">
-                                {/* <i className="fas fa-laugh-wink"></i> */}
-                            </div>
-                            <div className="sidebar-brand-text mx-3">GROUP2</div>
-                        </a>
-                        {/* <!-- Divider --> */}
-                        <hr className="sidebar-divider my-0" />
-                        {/* <!-- Nav Item - Dashboard --> */}
-                        <li className="nav-item active">
-                            <a className="nav-link" href="dashboard">
-                                <i className="fas fa-fw fa-tachometer-alt"></i>
-                                <span>Dashboard</span></a>
-                        </li>
-                        {/* <!-- Divider --> */}
-                        <hr className="sidebar-divider" />
-                        <li className="nav-item">
-                            <a className="nav-link" href="users">
-                                <i className="fas fa-fw fa-chart-area"></i>
-                                <span>Users</span></a>
-                        </li>
-                        {/* <!-- Nav Item - Tables --> */}
-                        <li className="nav-item">
-                            <a className="nav-link" href="order">
-                                <i className="fas fa-fw fa-table"></i>
-                                <span>Order</span></a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="Login">
-                                <i className="fas fa-fw fa-table"></i>
-                                <span>Sign in</span></a>
-                        </li>
+function Sidebar({ style, changeStyle }) {
+  return (
+    <ul className={style} id="accordionSidebar">
+      {/* <!-- Sidebar - Brand --> */}
+      <a
+        className="sidebar-brand d-flex align-items-center justify-content-center"
+        href="/"
+      >
+        <div className="sidebar-brand-icon rotate-n-15">
+          {/* <i className="fas fa-laugh-wink"></i> */}
+        </div>
+        <div className="sidebar-brand-text mx-3">GROUP2</div>
+      </a>
+      {/* <!-- Divider --> */}
+      <hr className="sidebar-divider my-0" />
+      {/* <!-- Nav Item - Dashboard --> */}
+      <li className="nav-item active">
+        <a className="nav-link" href="dashboard">
+          <i className="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span>
+        </a>
+      </li>
+      {/* <!-- Divider --> */}
+      <hr className="sidebar-divider" />
+      <li className="nav-item">
+        <a className="nav-link" href="users">
+          <i className="">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+  <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4"/>
+</svg>
+          </i>
+          <span>Users</span>
+        </a>
+      </li>
+      {/* <!-- Nav Item - Tables --> */}
+      <li className="nav-item">
+        <a className="nav-link" href="order">
+          <i className="">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-table" viewBox="0 0 16 16">
+  <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm15 2h-4v3h4zm0 4h-4v3h4zm0 4h-4v3h3a1 1 0 0 0 1-1zm-5 3v-3H6v3zm-5 0v-3H1v2a1 1 0 0 0 1 1zm-4-4h4V8H1zm0-4h4V4H1zm5-3v3h4V4zm4 4H6v3h4z"/>
+</svg>
+          </i>
+          <span>Order</span>
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="Login">
+          <i class=" me-3 fs-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-box-arrow-in-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z"
+              />
+              <path
+                fill-rule="evenodd"
+                d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
+              />
+            </svg>
+          </i>
 
-                        {/* <!-- Divider --> */}
-                        <hr className="sidebar-divider d-none d-md-block" />
+          <span>Sign in</span>
+        </a>
+      </li>
 
-                        {/* <!-- Sidebar Toggler (Sidebar) --> */}
-                        <div className="text-center d-none d-md-inline">
-                            <button className="rounded-circle border-0" id="sidebarToggle" onClick={changeStyle}></button>
-                        </div>
-                    </ul>
-    )
+      {/* <!-- Divider --> */}
+      <hr className="sidebar-divider d-none d-md-block" />
+
+      {/* <!-- Sidebar Toggler (Sidebar) --> */}
+      <div className="text-center d-none d-md-inline">
+        <button
+          className="rounded-circle border-0"
+          id="sidebarToggle"
+          onClick={changeStyle}
+        ></button>
+      </div>
+    </ul>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
