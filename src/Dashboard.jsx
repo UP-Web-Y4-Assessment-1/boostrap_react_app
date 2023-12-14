6f1b8c0f96a82902d7582c1f7edc1bec9b17bc07
Sophat <sophattann24@gmail.com>
2023-12-14 23:02:40 +0700

    update

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -33,44 +33,29 @@ function Dashboard() {
     return (
         <div>
             <body id="page-top">
-                {/* <!-- Page Wrapper --> */}
                 <div id="wrapper">
-                    {/* <!-- Sidebar --> */}
                     <Sidebar style={style} changeStyle={changeStyle}/>
-                    {/* <!-- End of Sidebar --> */}
 
-                    {/* <!-- Content Wrapper --> */}
                     <div id="content-wrapper" className="d-flex flex-column">
 
-                        {/* <!-- Main Content --> */}
                         <div id="content">
 
-                            {/* <!-- Topbar --> */}
                             <Navbar changeStyle1={changeStyle1}/>
-                            {/* <!-- End of Topbar --> */}
 
-                            {/* <!-- Begin Page Content --> */}
                             <Content/>
-                            {/* <!-- /.container-fluid --> */}
 
                         </div>
-                        {/* <!-- End of Main Content --> */}
-                        {/* <!-- Footer --> */}
+                       
                         <Footer/>
-                        {/* <!-- End of Footer --> */}
 
                     </div>
-                    {/* <!-- End of Content Wrapper --> */}
 
                 </div>
-                {/* <!-- End of Page Wrapper --> */}
 
-                {/* <!-- Scroll to Top Button--> */}
                 <a className="scroll-to-top rounded" href="#page-top">
                     <i className="fas fa-angle-up"></i>
                 </a>
 
-                {/* <!-- Logout Modal--> */}
                 <Logout/>
             </body>
         </div>
