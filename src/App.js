4660a872405f0038ffbfa673eec6f69964cb2bfb
Sengseyha HOEUNG <hoeungsengseyha168@gmail.com>
2023-12-14 23:01:16 +0700

    updatecode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Content from './components/Content';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
+import Logout from './components/Logout';
+import { useState } from 'react';
+import Order from './components/Order';
+import User from './components/User';
 
 function App() {
+  const [style, setStyle] = useState("navbar-nav bg-gradient-primary sidebar sidebar-dark accordion");
+
+  const changeStyle = () => {
+    if (style === "navbar-nav bg-gradient-primary sidebar sidebar-dark accordion") {
+      setStyle("navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled")
+    }
+    else {
+      setStyle("navbar-nav bg-gradient-primary sidebar sidebar-dark accordion")
+    }
+  };
+  const changeStyle1 = () => {
+    if (style === "navbar-nav bg-gradient-primary sidebar sidebar-dark accordion") {
+      setStyle("navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled1")
+    }
+    else {
+      setStyle("navbar-nav bg-gradient-primary sidebar sidebar-dark accordion")
+    }
+  };
   return (
-    <div className="App">
-      <a class="btn btn-primary" href="https://github.com/twbs/bootstrap/blob/main/.github/CONTRIBUTING.md#bug-reports" rel="noopener noreferrer" target="_blank">Read bug report guidelines</a>
-    </div>
+    <BrowserRouter>
+      <div>
+        <body id="page-top">
+          <div id="wrapper">
+            <Sidebar style={style} changeStyle={changeStyle} />
+            <div id="content-wrapper" className="d-flex flex-column">
+              <div id="content">
+                <Navbar changeStyle1={changeStyle1} />
+                <Routes>
+                  <Route path="/" element={<Content />} />
+                  <Route path="login" element={<Login />} />
+                  <Route path="order" element={<Order />} />
+                  <Route path="users" element={<User />} />
+                  <Route path="dashboard" element={<Content />} />
+                </Routes>
+
+              </div>
+              <Footer />              
+            </div>
+            
+
+          </div>
+
+          <a className="scroll-to-top rounded" href="#page-top">
+            <i className="fas fa-angle-up"></i>
+          </a>
+
+          <Logout />
+        </body>
+      </div>
+
+
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/User.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+function User() {
+    return (
+    <>
+    
+    <div className='p-5 bg-light '>
+        <div className='bg-white rounded  p-4'>
+            <table className='table caption-top '>
+                <thead>
+                    <tr>
+                        <th scope='col'>#</th>
+                        <th scope='col'>First</th>
+                        <th scope='col'>Last</th>
+                        <th scope='col'>Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope='row'>1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope='row'>2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope='row'>3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default User
